aac10c9fdb31d6ef160368fd0c4e889b982748a0
ggl <gonglei615lvv@gmail.com>
2020-06-07 16:32:33 +0800

    添加两数之和的解

---
+++ Algorithm/LeetCode/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  LeetCode
+//
+//  Created by ggl on 2020/6/7.
+//  Copyright © 2020 ggl. All rights reserved.
+//
+
+import Foundation
+
+print("LeetCode go go!")
---
+++ LeetCode/001-两数之和/001.swift
@@ -0,0 +1,28 @@
+//
+//  001.swift
+//  LeetCode
+//
+//  Created by ggl on 2020/6/7.
+//  Copyright © 2020 ggl. All rights reserved.
+//  001两数之和
+
+import Foundation
+
+/*
+ * 1. Two Sum
+ * 题意：返回数组中和为给定数的下标
+ * 难度：Easy
+ * 分类：Array, HashTable
+ * 算法：题目说明了数组中一定有解，且解唯一，所以用哈希表记录已遍历的元素即可
+ */
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var maps = [Int: Int]()
+    for (i, num) in nums.enumerated() {
+        if let index = maps[target - num] {
+            return [i, index]
+        }
+        maps[num] = i
+    }
+    return []
+}
